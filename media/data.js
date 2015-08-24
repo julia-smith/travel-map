ee1e5d3180a336caf1420ee7157ee76f7b8da554
Julia Smith <julia@julia-smith.com>
2015-08-23 23:49:00 -0700

    initial commit

---
+++ media/data.js
@@ -0,0 +1,162 @@
+var data = [
+    {
+        "event": "Homaha",
+        "location": "Omaha",
+        "startDate": "10-20-2014",
+        "endDate": "10-21-2014",
+        "type": "home",
+        "website": "http://julia-smith.com",
+        "from": "-",
+        "to": "-"
+    },
+    {
+        "event": "MozFest",
+        "location": "London",
+        "startDate": "10-21-2014",
+        "endDate": "10-27-2014",
+        "type": "attendee",
+        "website": "http://2014.mozillafestival.org/",
+        "from": "Omaha",
+        "to": "Omaha"
+    },
+    {
+        "event": "OpenNews Onboarding",
+        "location": "Los Angeles",
+        "startDate": "01-10-2015",
+        "endDate": "01-15-2015",
+        "type": "participant",
+        "website": "https://opennews.org/",
+        "from": "Omaha",
+        "to": "Omaha"
+    },
+    {
+        "event": "Center for Investigative Reporting",
+        "location": "San Francisco Bay Area",
+        "startDate": "02-17-2015",
+        "endDate": "03-03-2015",
+        "type": "home",
+        "website": "https://revealnews.org",
+        "from": "Omaha",
+        "to": "-"
+    },
+    {
+        "event": "NICAR",
+        "location": "Atlanta",
+        "startDate": "03-03-2015",
+        "endDate": "03-09-2015",
+        "type": "speaker",
+        "website": "http://ire.org/conferences/nicar2015/",
+        "from": "San Francisco Bay Area",
+        "to": "San Francisco Bay Area"
+    },
+    {
+        "event": "SNDMakes/SNDDC",
+        "location": "Washington, D.C.",
+        "startDate": "04-07-2015",
+        "endDate": "04-12-2015",
+        "type": "speaker",
+        "website": "http://www.snd.org/2015/02/sndmakes-at-snddc-how-might-we-better-tell-picture-stories/",
+        "from": "San Francisco Bay Area",
+        "to": "San Francisco Bay Area"
+    },
+    {
+        "event": "SmashingConf",
+        "location": "Los Angeles",
+        "startDate": "04-28-2015",
+        "endDate": "04-29-2015",
+        "type": "attendee",
+        "website": "http://smashingconf.com/la-2015/",
+        "from": "San Francisco Bay Area",
+        "to": "San Francisco Bay Area"
+    },
+    {
+        "event": "Visualized: Political Data",
+        "location": "Washington, D.C.",
+        "startDate": "05-20-2015",
+        "endDate": "05-22-2015",
+        "type": "attendee",
+        "website": "http://visualized.com/politicaldata/",
+        "from": "San Francisco Bay Area",
+        "to": "San Francisco Bay Area"
+    },
+    {
+        "event": "Google I/O",
+        "location": "San Francisco Bay Area",
+        "startDate": "05-28-2015",
+        "endDate": "05-29-2015",
+        "type": "attendee",
+        "website": "https://events.google.com/io2015/",
+        "from": "-",
+        "to": "-"
+    },
+    {
+        "event": "SRCCON",
+        "location": "Minneapolis",
+        "startDate": "06-25-2015",
+        "endDate": "06-26-2015",
+        "type": "speaker",
+        "website": "http://srccon.org/",
+        "from": "San Francisco Bay Area",
+        "to": "San Francisco Bay Area"
+    },
+    {
+        "event": "OpenNews Code Convening",
+        "location": "Portland",
+        "startDate": "07-22-2015",
+        "endDate": "07-25-2015",
+        "type": "participant",
+        "website": "https://opennews.org/what/community/convenings/",
+        "from": "San Francisco Bay Area",
+        "to": "San Francisco Bay Area"
+    },
+    {
+        "event": "NEJS Conf",
+        "location": "Omaha",
+        "startDate": "08-06-2015",
+        "endDate": "08-08-2015",
+        "type": "speaker",
+        "website": "https://nejsconf.com/",
+        "from": "San Francisco Bay Area",
+        "to": "San Francisco Bay Area"
+    },
+    {
+        "event": "Hacks/Hackers Media Party",
+        "location": "Buenos Aires",
+        "startDate": "08-27-2015",
+        "endDate": "08-29-2015",
+        "type": "speaker",
+        "website": "mediaparty.info",
+        "from": "San Francisco Bay Area",
+        "to": "San Francisco Bay Area"
+    },
+    {
+        "event": "ONA",
+        "location": "Los Angeles",
+        "startDate": "09-24-2015",
+        "endDate": "09-26-2015",
+        "type": "attendee",
+        "website": "ona15.journalists.org",
+        "from": "San Francisco Bay Area",
+        "to": "San Francisco Bay Area"
+    },
+    {
+        "event": "Code for America Summit",
+        "location": "San Francisco Bay Area",
+        "startDate": "09-30-2015",
+        "endDate": "10-02-2015",
+        "type": "attendee",
+        "website": "https://www.codeforamerica.org/summit/",
+        "from": "San Francisco Bay Area",
+        "to": "San Francisco Bay Area"
+    },
+    {
+        "event": "MozFest",
+        "location": "London",
+        "startDate": "11-06-2015",
+        "endDate": "11-08-2015",
+        "type": "speaker",
+        "website": "https://2015.mozillafestival.org/",
+        "from": "San Francisco Bay Area",
+        "to": "San Francisco Bay Area"
+    }
+]
---
+++ media/locations.js
@@ -0,0 +1,47 @@
+var locations = [
+    {
+        "location": "Omaha",
+        "x": 194,
+        "y": 118
+    },
+    {
+        "location": "London",
+        "x": 404,
+        "y": 84.15
+    },
+    {
+        "location": "Los Angeles",
+        "x": 140,
+        "y": 132
+    },
+    {
+        "location": "Atlanta",
+        "x": 214.81,
+        "y": 131.85000000000002
+    },
+    {
+        "location": "Washington, D.C.",
+        "x": 233.89000000000001,
+        "y": 118.6
+    },
+    {
+        "location": "San Francisco Bay Area",
+        "x": 137,
+        "y": 118.6
+    },
+    {
+        "location": "Minneapolis",
+        "x": 203,
+        "y": 102.7
+    },
+    {
+        "location": "Portland",
+        "x": 145,
+        "y": 105.35
+    },
+    {
+        "location": "Buenos Aires",
+        "x": 270.99,
+        "y": 310
+    }
+]
